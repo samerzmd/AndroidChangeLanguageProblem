a3676e9ee991a2367633c17e3cb495c8e50524bf
Sam.Z.D <samerzmd@gmail.com>
2016-03-29 19:47:41 +0200

    The  notSolved has been solved

--- app/src/notSolved/java/project/buildable/com/myapplication/MainActivity.java
+++ app/src/notSolved/java/project/buildable/com/myapplication/MainActivity.java
@@ -11,23 +11,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        String lang = App.getString(MainActivity.this, App.LANGUAGE);
-
-        App.updateLanguage(MainActivity.this, lang.equals("en") ? "ar" : "en");
-
         setContentView(R.layout.activity_main);
+
     }
 
     public void ChangeLanguage(View view) {
-        MainActivity.startActivity(this);
-    }
+        String lang = App.getString(MainActivity.this, App.LANGUAGE);
 
-    public static void startActivity(Context context) {
-        Intent intent = new Intent(context, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        context.startActivity(intent);
+        App.updateLanguage(MainActivity.this, lang.equals("en") ? "ar" : "en");
+        this.recreate();
     }
 
 }
