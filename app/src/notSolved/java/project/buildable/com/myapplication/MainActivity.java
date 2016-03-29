a690ad96cdeb04cb5ed93411ff0184b128dfe007
Sam.Z.D <samerzmd@gmail.com>
2016-03-29 19:50:01 +0200

    The  notSolved has been solved

--- app/src/notSolved/java/project/buildable/com/myapplication/MainActivity.java
+++ app/src/notSolved/java/project/buildable/com/myapplication/MainActivity.java
@@ -1,7 +1,5 @@
 package project.buildable.com.myapplication;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,12 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     public void ChangeLanguage(View view) {
         String lang = App.getString(MainActivity.this, App.LANGUAGE);
-
         App.updateLanguage(MainActivity.this, lang.equals("en") ? "ar" : "en");
         this.recreate();
     }
